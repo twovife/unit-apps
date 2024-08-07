20ec46956ca5c354e9aad76096d3cfd7896f7fde
twovife <78835047+twovife@users.noreply.github.com>
2024-08-07 13:57:43 +0700

    Update TransactionLoan.php

--- app/Models/TransactionLoan.php
+++ app/Models/TransactionLoan.php
@@ -10,7 +10,7 @@ class TransactionLoan extends Model
     use HasFactory;
 
     protected $fillable = [
-        "transaction_manage_customers", "branch_id", "request_date", "check_date", "drop_date", "hari", "pinjaman_ke", "status", "notes", "user_input",
+        "transaction_manage_customers", "branch_id", "request_date", "check_date", "drop_date", "hari", "pinjaman_ke", "status", "notes", "user_input", "old_id",
     ];
 
     public function loan_instalment()
