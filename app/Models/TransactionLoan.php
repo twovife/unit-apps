2844665a2b3d16a8f28c65be68b9bbbacf5bd23c
twovife <78835047+twovife@users.noreply.github.com>
2024-08-23 13:45:13 +0700

    asd

--- app/Models/TransactionLoan.php
+++ app/Models/TransactionLoan.php
@@ -7,37 +7,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransactionLoan extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable = [
-        "transaction_manage_customer_id",
-        "branch_id",
-        "request_date",
-        "check_date",
-        "drop_date",
-        "hari",
-        "pinjaman_ke",
-        "status",
-        "notes",
-        "user_input",
-        "old_id",
-    ];
+  protected $fillable = [
+    "transaction_manage_customer_id",
+    "branch_id",
+    "request_date",
+    "check_date",
+    "drop_date",
+    "hari",
+    "pinjaman_ke",
+    "status",
+    "notes",
+    "user_input",
+    "old_id",
+    "drop_before",
+    "request_nominal",
+    "approved_nominal",
+    "drop",
+    "pinjaman",
+  ];
 
-    public function loan_instalment()
-    {
-        return $this->hasMany(TransactionLoanInstalment::class, 'transaction_loan_id', 'id');
-    }
-    public function loan_nominal()
-    {
-        return $this->hasOne(TransactionLoanNominal::class, 'transaction_loan_id', 'id');
-    }
+  public function loan_instalment()
+  {
+    return $this->hasMany(TransactionLoanInstalment::class, 'transaction_loan_id', 'id');
+  }
 
-    public function manage_customer()
-    {
-        return $this->belongsTo(TransactionManageCustomer::class, 'transaction_manage_customer_id', 'id');
-    }
-    public function branch()
-    {
-        return $this->belongsTo(Branch::class, 'branch_id', 'id');
-    }
+  public function manage_customer()
+  {
+    return $this->belongsTo(TransactionManageCustomer::class, 'transaction_manage_customer_id', 'id');
+  }
+  public function branch()
+  {
+    return $this->belongsTo(Branch::class, 'branch_id', 'id');
+  }
 }
