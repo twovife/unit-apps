a147a3381de958c0c4fb377bac640c63ac15b170
twovife <78835047+twovife@users.noreply.github.com>
2026-01-19 02:07:43 +0700

    Update BatchInputV2Seeder.php

--- database/seeders/BatchInputV2Seeder.php
+++ database/seeders/BatchInputV2Seeder.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class BatchInputSeeder extends Seeder
+class BatchInputV2Seeder extends Seeder
 {
   /**
    * Run the database seeds.
