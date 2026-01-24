da300b8775b36135f9b9096797de3390fa141a5b
twovife <78835047+twovife@users.noreply.github.com>
2026-01-24 10:15:05 +0700

    push

--- database/seeders/BatchInputV2Seeder.php
+++ database/seeders/BatchInputV2Seeder.php
@@ -44,7 +44,7 @@ class BatchInputV2Seeder extends Seeder
   public function run(): void
   {
 
-    $nasabahRaw = collect(json_decode(file_get_contents(storage_path('rembang1sr.json'))));
+    $nasabahRaw = collect(json_decode(file_get_contents(storage_path('patriss.json'))));
 
     // Pre-process JSON dulu (biar gak hitung carbon/helper berulang kali)
 
@@ -60,7 +60,7 @@ class BatchInputV2Seeder extends Seeder
     });
 
 
-    $id_branch = 65;
+    $id_branch = 70;
     $id_mantri_default = 2565;
 
 
