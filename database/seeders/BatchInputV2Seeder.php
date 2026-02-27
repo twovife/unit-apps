b76780acc1e48869b8f00184368c0c6dd54bc76f
twovife <78835047+twovife@users.noreply.github.com>
2026-02-27 14:37:37 +0700

    asdsad

--- database/seeders/BatchInputV2Seeder.php
+++ database/seeders/BatchInputV2Seeder.php
@@ -42,7 +42,7 @@ class BatchInputV2Seeder extends Seeder
   public function run(): void
   {
 
-    $nasabahRaw = collect(json_decode(file_get_contents(storage_path('jumatpos5.json'))));
+    $nasabahRaw = collect(json_decode(file_get_contents(storage_path('seninpos5.json'))));
 
     // Pre-process JSON dulu (biar gak hitung carbon/helper berulang kali)
     $nasabah = $nasabahRaw
