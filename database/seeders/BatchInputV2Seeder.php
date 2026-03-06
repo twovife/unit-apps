e6ca67807c0c737ddf2d0ea0563b142660619479
twovife <78835047+twovife@users.noreply.github.com>
2026-03-07 03:22:03 +0700

    adasd

--- database/seeders/BatchInputV2Seeder.php
+++ database/seeders/BatchInputV2Seeder.php
@@ -42,7 +42,7 @@ class BatchInputV2Seeder extends Seeder
   public function run(): void
   {
 
-    $nasabahRaw = collect(json_decode(file_get_contents(storage_path('sabtupos1.json'))));
+    $nasabahRaw = collect(json_decode(file_get_contents(storage_path('sabtupos4.json'))));
 
     // Pre-process JSON dulu (biar gak hitung carbon/helper berulang kali)
     $nasabah = $nasabahRaw
@@ -78,11 +78,11 @@ class BatchInputV2Seeder extends Seeder
     // $id_mantri_default = 2400;
 
     // solo 1
-    $id_branch = 75;
+    // $id_branch = 75;
     // solo 2
     // $id_branch = 76;
     // solo 4
-    // $id_branch = 78;
+    $id_branch = 78;
     $id_mantri_default = 2181;
 
     $totalBatch = ceil($nasabah->count() / 100);
